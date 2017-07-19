117accec13d858e0ef7e871678983ddc14333a0d
Varun Santhanam <varun.santhanam@sherbit.io>
2017-07-19 07:29:01 -0700

    Initial Commit

---
+++ ScoreFive/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  ScoreFive
+//
+//  Created by Varun Santhanam on 7/19/17.
+//  Copyright © 2017 Varun Santhanam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ ScoreFive/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ScoreFive
+//
+//  Created by Varun Santhanam on 7/19/17.
+//  Copyright © 2017 Varun Santhanam. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
